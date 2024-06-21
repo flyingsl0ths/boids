561fbc49bf27159eb4c047be1b0a4caa4b440ba2
flyingsl0ths <randomemail33110@protonmail.com>
2024-06-21 13:44:04 -0700

    Add draw func. getter

--- simu/shape.lua
+++ simu/shape.lua
@@ -12,6 +12,26 @@ function Shapes.random()
 	return math.random(Shapes.triangle, Shapes.circle)
 end
 
+function Shape.draw_function_of(ctx, boid_shape)
+	if boid_shape == Shapes.triangle then
+		return function(x, y)
+			ctx.polygon("fill", x, y, x + 10, y + 10, x - 10, y + 10)
+		end
+	elseif boid_shape == Shapes.circle then
+		return function(x, y)
+			ctx.circle("fill", x, y, 10)
+		end
+	elseif boid_shape == Shapes.square then
+		return function(x, y)
+			ctx.rectangle("fill", x, y, 20, 20)
+		end
+	else
+		return function(x, y)
+			ctx.line(x, y, x + 10, y + 10)
+		end
+	end
+end
+
 return setmetatable({}, {
 	__index = Shapes,
 	__newindex = function(_, _, _)
