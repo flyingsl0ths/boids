69b4f1abdc6e7964c3c1dd27aca35bc64a9e2a4c
flyingsl0ths <randomemail33110@protonmail.com>
2024-07-15 06:15:11 -0700

    Move boid funcs to module

--- main.lua
+++ main.lua
@@ -4,46 +4,17 @@ local shapes = require "simu.shape"
 
 
 local boids = {}
-local windowSize = { width = 800, height = 600 }
-local number_of_boids = 130
-local boid_size = 200
 
--- NOTE: This is the distance at which boids will start to interact with each other
-local visual_range = 75
+local WINDOW_SIZE = { width = 800, height = 600 }
+
+local STARTING_AMOUNT = 150
+
 
 local function positionBoids()
-  for i = 1, number_of_boids do
-    local position = vec2(math.random(windowSize.width), math.random(windowSize.height))
+  for i = 1, STARTING_AMOUNT do
+    local position = vec2(math.random(WINDOW_SIZE.width), math.random(WINDOW_SIZE.height))
     local bd = boid(position, vec2.randomUnit() - vec2(6), shapes.circle)
     boids[i] = bd
-  end
-end
-
-local function boundPosition(bd)
-  local turn_factor = 1
-
-  if bd.position.x < boid_size then
-    bd.velocity.x = bd.velocity.x + turn_factor
-  end
-  if bd.position.x > windowSize.width - boid_size then
-    bd.velocity.x = bd.velocity.x - turn_factor
-  end
-  if bd.position.y < boid_size then
-    bd.velocity.y = bd.velocity.y + turn_factor
-  end
-  if bd.position.y > windowSize.height - boid_size then
-    bd.velocity.y = bd.velocity.y - turn_factor
-  end
-end
-
-local function limitSpeed(bd)
-  local speed_limit = 10.25
-
-  local speed = bd.velocity:length()
-
-  if speed > speed_limit then
-    bd.velocity.x = (bd.velocity.x / speed) * speed_limit
-    bd.velocity.y = (bd.velocity.y / speed) * speed_limit
   end
 end
 
@@ -52,90 +23,24 @@ local function drawBoids()
   for i = 1, #boids do
     local bd         = boids[i]
     local draw_shape = shapes.drawFuncOf(bd.shape)
-    local v          = bd.velocity:length()
-    local c          = love.math.noise(v)
-    ctx.setColor(c, c, c)
+    local velocity   = bd.velocity:length()
+    local color      = love.math.noise(velocity)
+    ctx.setColor(color, color, color)
     draw_shape(ctx, bd.position.x, bd.position.y)
   end
 end
 
-local function flyTowardsCenter(b_j, bds)
-  -- Rule 1: Boids try to fly towards the center of mass of neighboring boids
-  -- NOTE:: The center is just the average position of all the boids
 
-  -- NOTE: This controls how much/fast to move towards the center
-  -- NOTE: An easing function can be used here
-  local centering_factor = 0.0010
-
-  local center           = vec2()
-  local neighbor_count   = 0
-
-  for i = 1, #bds do
-    local b_n = bds[i]
-    if b_j.position:distance(b_n.position) < visual_range then
-      center = center + b_n.position
-      neighbor_count = neighbor_count + 1
-    end
-  end
-
-  if neighbor_count ~= 0 then
-    center = center / neighbor_count
-    b_j.velocity = b_j.velocity + (center - b_j.position):scaled(centering_factor)
-  end
-end
-
-local function avoidOthers(bd_j, bds)
-  -- Rule 2: Boids try to keep a small distance away from other objects (including other boids)
-  -- NOTE: This is done by moving a boid away from other boids if they are too close only by
-  -- a percentage of the final accmulated distance
-  local min_distance = 25
-
-  -- NOTE: This controls the percentage of the final distance to move away from the other boids
-  local avoid_factor = 10 / 100
-
-  local movement = vec2()
-
-  for i = 1, #bds do
-    local b_n = bds[i]
-    if b_n ~= bd_j and bd_j.position:distance(b_n.position) < min_distance then
-      movement = movement + (bd_j.position - b_n.position)
-    end
-  end
-
-  bd_j.velocity = bd_j.velocity + movement:scaled(avoid_factor)
-end
-
-local function matchVelocity(b_j, bds)
-  -- Rule 3: Boids try to match velocity with near boids
-  -- NOTE: The controls the percentage of the final velocity
-  local matching_factor = 2 / 100
-
-  local average_velocity = vec2()
-  local num_neighbors = 0
-
-  for i = 1, #bds do
-    local b_n = bds[i]
-    if b_j.position:distance(b_n.position) < visual_range then
-      average_velocity = average_velocity + b_n.velocity
-      num_neighbors = num_neighbors + 1
-    end
-  end
-
-  if num_neighbors ~= 0 then
-    average_velocity = average_velocity / num_neighbors
-    b_j.velocity = b_j.velocity + (average_velocity - b_j.velocity):scaled(matching_factor)
-  end
-end
 
 local function moveToNewPositions()
   for i = 1, #boids do
     local bd = boids[i]
 
-    flyTowardsCenter(bd, boids)
-    avoidOthers(bd, boids)
-    matchVelocity(bd, boids)
-    limitSpeed(bd)
-    boundPosition(bd)
+    bd:flyTowardsCenter(boids)
+    bd:avoidOthers(boids)
+    bd:matchVelocity(boids)
+    bd:limitSpeed()
+    bd:boundPosition(WINDOW_SIZE)
 
     bd.position = bd.position + bd.velocity
   end
@@ -143,7 +48,7 @@ end
 
 function love.load()
   love.window.setTitle("Flocking Boids")
-  love.window.setMode(windowSize.width, windowSize.height, {})
+  love.window.setMode(WINDOW_SIZE.width, WINDOW_SIZE.height, {})
   positionBoids()
 end
 
--- simu/boid.lua
+++ simu/boid.lua
@@ -1,10 +1,15 @@
-local vec2   = require "simu.vec2"
-local shapes = require "simu.shape"
-local utils  = require "simu.utils"
+local vec2         = require "simu.vec2"
+local shapes       = require "simu.shape"
+local utils        = require "simu.utils"
 
-local Boid   = {}
+local BOID_SIZE    = 120
 
-Boid.__index = Boid
+-- NOTE: This is the distance at which boids will start to interact with each other
+local VISUAL_RANGE = 75
+
+local Boid         = {}
+
+Boid.__index       = Boid
 
 local function new(pos, vel, shape)
 	local b = {
@@ -13,6 +18,102 @@ local function new(pos, vel, shape)
 		shape = shape or shapes.random()
 	}
 	return setmetatable(b, Boid)
+end
+
+function Boid:boundPosition(bounds)
+	local turn_factor = 1
+
+	if self.position.x < BOID_SIZE then
+		self.velocity.x = self.velocity.x + turn_factor
+	end
+	if self.position.x > bounds.width - BOID_SIZE then
+		self.velocity.x = self.velocity.x - turn_factor
+	end
+	if self.position.y < BOID_SIZE then
+		self.velocity.y = self.velocity.y + turn_factor
+	end
+	if self.position.y > bounds.height - BOID_SIZE then
+		self.velocity.y = self.velocity.y - turn_factor
+	end
+end
+
+function Boid:limitSpeed()
+	local speed_limit = 5.25
+
+	local speed = self.velocity:length()
+
+	if speed > speed_limit then
+		self.velocity.x = (self.velocity.x / speed) * speed_limit
+		self.velocity.y = (self.velocity.y / speed) * speed_limit
+	end
+end
+
+function Boid:flyTowardsCenter(boids)
+	-- Rule 1: Boids try to fly towards the center of mass of neighboring boids
+	-- NOTE:: The center is just the average position of all the boids
+
+	-- NOTE: This controls how much/fast to move towards the center
+	-- NOTE: An easing function can be used here
+	local centering_factor = 0.0010
+
+	local center           = vec2()
+	local neighbor_count   = 0
+
+	for i = 1, #boids do
+		local other = boids[i]
+		if self.position:distance(other.position) < VISUAL_RANGE then
+			center = center + other.position
+			neighbor_count = neighbor_count + 1
+		end
+	end
+
+	if neighbor_count ~= 0 then
+		center = center / neighbor_count
+		self.velocity = self.velocity + (center - self.position):scaled(centering_factor)
+	end
+end
+
+function Boid:avoidOthers(boids)
+	-- Rule 2: Boids try to keep a small distance away from other objects (including other boids)
+	-- NOTE: This is done by moving a boid away from other boids if they are too close only by
+	-- a percentage of the final accmulated distance
+	local min_distance = 25
+
+	-- NOTE: This controls the percentage of the final distance to move away from the other boids
+	local avoid_factor = 10 / 100
+
+	local movement = vec2()
+
+	for i = 1, #boids do
+		local b_n = boids[i]
+		if b_n ~= self and self.position:distance(b_n.position) < min_distance then
+			movement = movement + (self.position - b_n.position)
+		end
+	end
+
+	self.velocity = self.velocity + movement:scaled(avoid_factor)
+end
+
+function Boid:matchVelocity(boids)
+	-- Rule 3: Boids try to match velocity with near boids
+	-- NOTE: The controls the percentage of the final velocity
+	local matching_factor = 2 / 100
+
+	local average_velocity = vec2()
+	local num_neighbors = 0
+
+	for i = 1, #boids do
+		local other = boids[i]
+		if self.position:distance(other.position) < VISUAL_RANGE then
+			average_velocity = average_velocity + other.velocity
+			num_neighbors = num_neighbors + 1
+		end
+	end
+
+	if num_neighbors ~= 0 then
+		average_velocity = average_velocity / num_neighbors
+		self.velocity = self.velocity + (average_velocity - self.velocity):scaled(matching_factor)
+	end
 end
 
 return setmetatable(Boid, {
